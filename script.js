f62ff75764d4407a7710dde43b80aea89b2ac4f6
Lucas <lucas_s_wisniewski@estudante.sesisenai.org.br>
2025-03-20 09:36:30 -0300

    add

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-int1 = parseInt(prompt("Digite um número inteiro: "));
-
-function ehPar(n) {
-    return n % 2 == 0;
+/*
+console.log('hello world');
+let numero = prompt('Digite seu número:');
+function verificarPar(numero){
+    if(numero % 2 === 0 ){
+        console.log(numero + " é igual a par");
+    }else{
+        console.log(numero + " é igual a ímpar");
+    }
 }
-
-if (ehPar(int1)) {
-    console.log("O número " + int1 + " é par");
-    console.log("Virou impar: " + (int1 + 1));
-} else {
-    console.log("O número " + int1 + " é impar");
-    console.log("Virou par: " + (int1 + 1));
-}
+verificarPar(numero);
+*/
