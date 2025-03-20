35ee46d736daade80d5ca3bc449eda9bd33d015a
Lucas <lucas_s_wisniewski@estudante.sesisenai.org.br>
2025-03-20 09:53:22 -0300

    add

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ function verificarPar(numero){
 }
 verificarPar(numero);
 */
+let n1 = parseFloat (prompt('escreva n1:'))
+let n2 = parseFloat (prompt('escreva n2'))
+
+function calcular (num1, num2) {
+const soma = num1 + num2;
+const subtracao = num1 - num2
+const multiplicacao = num1 * num2;
+console.log (soma)
+console.log (subtracao)
+console.log (multiplicacao)
+if (num2 === 0) {
+    console.log("não da pra dividir por 0!")
+} else{
+    const divisao = num1 / num2;
+    console.log(divisao)
+}
+}
+
+calcular(n1,n2);
